92648fac74d57682b996ab45681d4a4e7495518a
butlerwang <butlerwang@gmail.com>
2018-06-26 03:57:03 +0000

    Some changes

--- qsort/qsort_test.go
+++ qsort/qsort_test.go
@@ -2,7 +2,7 @@ package qsort
 
 import "testing"
 
-func TestQuickSort1(t testing.T) {
+func TestQuickSort1(t *testing.T) {
 	values := []int{5, 4, 3, 2, 1}
 	QuickSort(values)
 	if values[0] != 1 || values[1] != 2 || values[2] != 3 || values[3] != 4 || values[4] != 5 {
@@ -10,7 +10,7 @@ func TestQuickSort1(t testing.T) {
 	}
 }
 
-func TestQuickSort2(t testing.T) {
+func TestQuickSort2(t *testing.T) {
 	values := []int{5, 5, 3, 2, 1}
 	QuickSort(values)
 	if values[0] != 1 || values[1] != 2 || values[2] != 3 || values[3] != 5 || values[4] != 5 {
@@ -18,7 +18,7 @@ func TestQuickSort2(t testing.T) {
 	}
 }
 
-func TestQuickSort3(t testing.T) {
+func TestQuickSort3(t *testing.T) {
 	values := []int{5}
 	QuickSort(values)
 	if values[0] != 5 {
